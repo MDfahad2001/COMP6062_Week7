30c4f38169227fc6a4346683dd1795f7a140204b
Fahad <fahadjan202001@gmail.com>
2025-06-16 18:34:44 -0400

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
-const NUMBER = document.querySelector("numberInput");
+const number = document.querySelector("numberInput");
 
+number.addEventLister("input",conductionChecker);
+
+function conductionChecker()
+{
 if(NUMBER<=10||NUMBER>=-10)
 {
     alert("The number should be between -10 and 10");
 }
+}
+
 
 const fruit ={
     name: "mango",
